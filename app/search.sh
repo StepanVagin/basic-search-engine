2f0dc093105e3202dd5bebd16b3ce170c53d6a9e
stepan <step.way.a12@gmail.com>
2026-04-11 15:41:38 +0300

    spark bug fix

--- app/search.sh
+++ app/search.sh
@@ -9,21 +9,14 @@ fi
 
 source .venv/bin/activate
 
-# Python of the driver (/app/.venv/bin/python)
 export PYSPARK_DRIVER_PYTHON=$(which python)
 
-# Python of the executor (./.venv/bin/python)
-export PYSPARK_PYTHON=./.venv/bin/python
-
 spark-submit \
-    --master yarn \
-    --deploy-mode client \
+    --master local[*] \
     --driver-memory 2g \
-    --executor-memory 2g \
     --packages com.datastax.spark:spark-cassandra-connector_2.12:3.4.1 \
     --conf spark.cassandra.connection.host=cassandra-server \
     --conf spark.cassandra.connection.port=9042 \
     --conf spark.sql.extensions=com.datastax.spark.connector.CassandraSparkExtensions \
     --conf spark.sql.catalog.cassandra=com.datastax.spark.connector.datasource.CassandraCatalog \
-    --archives /app/.venv.tar.gz#.venv \
     query.py "$@"
